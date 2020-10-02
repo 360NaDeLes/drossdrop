add10466b9ee61abab58eae20b31b38f1ebdad56
Raphaël van Marion <raphaeldarealmvp@gmail.com>
2020-10-02 10:59:23 +0200

    Added layers and DTOs

---
+++ DrossDrop.DTOs/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrossDrop.DTOs
+{
+    public class ProductDTO
+    {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public decimal productPrice { get; set; }
+
+    }
+}
---
+++ DrossDrop.DTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrossDrop.DTOs
+{
+    class UserDTO
+    {
+        public int userId { get; set; }
+        public int roleId { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
